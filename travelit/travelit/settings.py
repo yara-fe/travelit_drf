60b3da06ca87c4509cc8db7d31d13a1b6b05b2a6
yara-fe <yarafe95@gmail.com>
2023-08-08 11:15:40 +1000

    Created user app and saved in settings.py

--- travelit/travelit/settings.py
+++ travelit/travelit/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'itineraries.apps.ItinerariesConfig',
+    'users.apps.UsersConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
