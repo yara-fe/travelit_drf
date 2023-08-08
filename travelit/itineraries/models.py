ceb33d0261f35adbeb62aeed2449c0bfb268e11b
yara-fe <yarafe95@gmail.com>
2023-08-08 11:44:43 +1000

    Added ForeignKey relationship in models.py

--- travelit/itineraries/models.py
+++ travelit/itineraries/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model #reference to user model
 
 class Itinerary(models.Model):
     title = models.CharField(max_length=200)
@@ -10,15 +11,24 @@ class Itinerary(models.Model):
     itinerary_notes = models.TextField()
     image = models.URLField()
     date_created = models.DateTimeField()
-    creator = models.CharField(max_length=200) #user who created the itinerary
+    #user who created the itinerary
+    creator = models.ForeignKey(
+        get_user_model(), #related model
+        on_delete=models.CASCADE,
+        related_name='owned_itineraries' #sets up property in get_user_model
+    ) 
 
 
 class Reward(models.Model):
     amount = models.IntegerField
-    giver = models.CharField(max_length=200)
+    giver = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='rewards'
+    ) 
     anonymous = models.BooleanField()
     itinerary_id = models.ForeignKey(
-        'Itinerary', #model whic Reward is related to
+        'Itinerary', #model which Reward is related to
         on_delete=models.CASCADE, #delete Rewards if Itinerary is deleted
         related_name='rewards' #allows Django to set up a "property" in Itinerary model
     )
