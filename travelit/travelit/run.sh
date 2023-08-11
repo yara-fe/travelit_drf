5ea9c582a4aa50f8f3aeab575af6d0de3b46daf7
yara-fe <yarafe95@gmail.com>
2023-08-12 09:55:33 +1000

    deployment and screenshots

--- travelit/travelit/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python manage.py migrate
-python manage.py createsuperuser --no-input
-gunicorn --bind :8000 --workers 1 crowdfunding.wsgifly volumes create -a <app-name> -r syd --size 1 dbvol
